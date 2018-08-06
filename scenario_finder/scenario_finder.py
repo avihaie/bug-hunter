5d8d57bc039f5ae0969904c602f7bdce771faa2f
Ilan Zuckerman <izuckerm@redhat.com>
2018-08-06 14:16:55 +0300

    Junk Cleanups and adding docstrings

--- scenario_finder/scenario_finder.py
+++ scenario_finder/scenario_finder.py
@@ -1,14 +1,7 @@
-import logging, os, datetime, socket, sys, tarfile, gzip
+import logging, os, datetime, sys, argparse
 from traceback import print_stack
-import argparse
-from rrmngmnt.host import Host
 
-LOCALHOST_LOGS_PATH = os.path.expanduser("~/tmp")
-EXTRACTED_FOLDER_NAME = 'extracted'
-# this monstrous thing extracts localhost ip
-LOCAL_HOST = Host( [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
-                    if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0],
-                    s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0] )
+EXTRACTED_FOLDER_NAME = 'extracted_gz'
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,6 +12,7 @@ logger = logging.getLogger(__name__)
 
 class ScenarioFinder:
     """
+    Class for searching and storing the needed events from engine logs. can handle gz files as well as plain text files.
     """
     def __init__(self, time_start, path_logs, event_string, scenario_result_file_path):
         self.time_start = time_start[:-3]  # cutting seconds. '2018-07-16 03:39:04 --> '2018-07-16 03:39'
@@ -29,9 +23,12 @@ class ScenarioFinder:
         self.scenario_result_file_path = scenario_result_file_path
 
     def parse_logs(self):
+        """
+        Main method where all of parsing logic's begins. It calls other methods for different tasks.
+        """
         logger.info('Starting to parse files in ' + self.path_logs)
 
-        self.check_dir_exists(self.path_logs)
+        self.check_log_dir_exists(self.path_logs)
 
         all_log_files_lst = os.listdir(self.path_logs)
         engine_log_files_lst = [x for x in all_log_files_lst if 'engine' in x]
@@ -56,13 +53,16 @@ class ScenarioFinder:
                         if self.time_start_found:
                             self.find_event_string_in_line(full_file_name, line)
 
-            except IOError:
-                logger.error("File does not appear to exist" + full_file_name)
+            except IOError as e:
+                logger.error("File does not appear to exist: %s\n %s" % (full_file_name, e))
 
         logger.info('Finished parsing logs, about to dump the scenario to: ' + self.scenario_result_file_path)
-        self.dumb_scenario_list_to_file()
+        self.dump_scenario_list_to_file()
 
-    def dumb_scenario_list_to_file(self):
+    def dump_scenario_list_to_file(self):
+        """
+        Writes the list of found event lines (which were extracted from logs) to a txt file.
+        """
         try:
             with open(self.scenario_result_file_path, 'w') as f:
                 f.writelines(self.events_found_lst)
@@ -71,27 +71,48 @@ class ScenarioFinder:
             logger.error("Failed to dump scenarios list to file: %s \n %s" % (self.scenario_result_file_path, e))
 
     def find_event_string_in_line(self, full_file_name, line):
+        """
+        Finds the events string inside a line from some log file, and puts it to a list.
+        :param full_file_name: Full path to a plain text file (could be extracted GZ). used only for logging here.
+        :param line: Line from a file which needs to be parsed for the events string.
+        """
         if self.event_string_to_find in line:
             logger.debug('Found string: %s inside line. appending it to events_found_lst. File: %s The line: %s'
                          % (self.event_string_to_find, full_file_name, line))
             self.events_found_lst.append(line)
 
     def find_time_start_string_in_line(self, line, full_file_name):
+        """
+        Finds the start time string inside a line from a log file. Returns true if found, false if not found.
+        :param line: Line from a file which needs to be parsed for the time string.
+        :param full_file_name: full_file_name: Full path to a plain text file (could be extracted GZ).
+        Used only for logging here.
+        :return:  true if found, false if not found.
+        """
         if self.time_start in line:
             logger.info("Found start time string: %s in file: %s Beginning to search for event string: %s"
                         % (self.time_start, os.path.basename(full_file_name), self.event_string_to_find))
             return True
         return False
 
-    def check_dir_exists(self, dir_path):
+    def check_log_dir_exists(self, dir_path):
+        """
+        Checks logs dir for existence. Log dir is a directory for holding all of the log files for parsing.
+        Exits the program if the directory does not exist.
+        :param dir_path: Full path of the directory.
+        """
         #  checking existence of logs directory which we will parse
         if not os.path.isdir(dir_path):
             logger.error("Not a valid directory on local machine: " + dir_path)
             sys.exit()
 
     def extract_gz_file(self, full_file_name):
-        # logger.info("Attempting to extract " + full_file_name)
-        # TODO: EXTRACTED_FOLDER_NAME
+        """
+        Extracts a gz file to folder name as defined in 'EXTRACTED_FOLDER_NAME' inside the folder where the logs reside.
+        For example: '/home/ilan/tmp/bughunter/extracted_gz/'
+        :param full_file_name: Full path to a GZ file.
+        :return: Full path to the extracted file inside 'extracted' folder.
+        """
         full_folder_path_for_extracted_file = os.path.join(self.path_logs, EXTRACTED_FOLDER_NAME)  # e.g /some/dir/extracted
 
         if not os.path.exists(full_folder_path_for_extracted_file):
@@ -114,27 +135,21 @@ class ScenarioFinder:
         return full_file_path_for_extracted_file
 
 
-
 def main():
     """
     In case of manual execution -
-    (event,event_details,mail_source,mail_user,mail_password)
 
-    e.g python notifier.py -e Exception -d "NullPointerException" -u "testuser@gmail.com" -p "123456" -o
-    "storage-ge4-test.scl.lab.tlv.redhat.com" -t "TestCase18145"
-
-        - event: summery of the event
-        - event_details: details of the event
-        - mail_user = mail_source: email source address that will appear on the sent mail triggered after the event
-        - mail_password: email password of the source address
+    e.g python scenario_finder.py -t "2018-07-25 03:08:41" -p "/home/ilan/tmp/bughunter" -e "EVENT_ID"
 
     Options -
-        * -e, --event : summery of the event (e.g. -e "Exception caught" )
-        * -d, --event_details: event details (e.g. -d "NullPointerException ...")
-        * -u, --mail_user : email user which is equal to email itself (e.g. -u "test@gmail.com")
-        * -p, --mail_password : email password (e.g. -p "123456")
-        * -o, --host_name : host name the issue occurred on (e.g. -p "storage-ge4-test.scl.lab.tlv.redhat.com")
-        * -t, --test_name : name of the test (e.g. -e 'TestCase18145')
+        * -t, --time_start : Test start time. states starting from which time we should search for events. it must be in
+                            the following format: '%Y-%m-%d %H:%M:%S'
+        * -p, --path_logs : Full path of logs folder on local machine.
+        * -e, --events_to_grab : String for which we need to search inside log files. When found, the line is stored
+                                and written to output file. In Engine log this would be 'EVENT_ID'
+        * -s, --scenario_result_file_path : Full path to a file in which we want to store the events list. By default
+                                            It would be: /tmp/scenario_file_<date_time_now>.txt Where date time format
+                                            is '%Y-%m-%d_%H:%M:%S'.
 
     """
     datetime_now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
@@ -149,7 +164,7 @@ def main():
     parser.add_argument("-p", "--path_logs", action="store", dest="path_logs",
                         help="Full LOCAL folder path of logs that need to be parsed.", required=True)
 
-    parser.add_argument("-e", "--events_to_grap", action="store", dest="event_string",
+    parser.add_argument("-e", "--events_to_grab", action="store", dest="event_string",
                         help="This is the string we are looking for, inside the logs", required=True)
 
     parser.add_argument("-s", "--scenario_result_file_path", action="store", help="Full path for a result file where we"
