b916ff60f437cad96ec7dc58e3d32ecaa56693ef
Avihai Efrat <avichaiefrat@gmail.com>
2018-10-10 11:52:46 +0300

    Update manager.py

--- manager/manager.py
+++ manager/manager.py
@@ -21,13 +21,16 @@ logger = logging.getLogger(__name__)
 class Manager:
     """
     responsibilities:
-    1) Lunch the main script that will trigger bug hunter operation
-    2) log-listening to which hosts & logs on those hosts
-    3) trigger local/remote dump logs
-    4) enable/disable mapping (deep analisys)
-    5) fault-handling of log-listener and other operations when host/service was restarted/unavailable
+    1. Lunch the main script that will trigger bug hunter operation Rhv/other flow.
+    2. Log-listening to which hosts & logs on those hosts.
+    3. Collect relevant logs and fetch them to localhost.
+    4. Notify via mail and consule when an issue occurs.
+    5. Check the enviroment state staring the test and also after an issue occurs.
+    6. Create a bugzilla ready report.
+    7. enable/disable mapping (deep analisys) - TBD
+    8. fault-handling of log-listener and other operations when host/service was restarted/unavailable - TBD
 
-    rhv_manager method will run the main flow for handling an rhv issue
+    rhv_manager method will run the main flow for handling an rhv issue, other(openstack,CNV ) currently are TBD.
     """
 
     def __init__(
